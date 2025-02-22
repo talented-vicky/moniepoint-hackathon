acdbd738a44f6cee8e3e96791cfc975db249aa6a
talented_vicky <victorotubure7@gmail.com>
2025-02-22 14:40:41 +0100

    got highest sales volume and highest sales value

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [dailySales, setDailySales] = useState<string[]>([]);
+  const [highestSale, setHighestSale] =  useState<number | null>(null);
+  const [productsSold, setProductsSold] =  useState<string[]>([])
+  const [highestQty, setHighestQty] = useState<number | null>(null);
+
+  useEffect(() => {
+    const dataCalls = [
+      fetchFileInfo()
+    ];
+    Promise.all(dataCalls).finally(() => {})
+  }, [])
+
+  const fetchFileInfo = async () => {
+    try {
+      const data = await fetch('/accounting-data/2025-01-01.txt');
+      const textFile = await data.text();
+
+      const line = textFile.split('\n');
+
+      const total = line.map(items => {
+        const lineItem = items.split(',')
+        const salesStaffId =  lineItem[0];
+        const transactionTime =  lineItem[1];
+
+
+        const productSold =  lineItem[2];
+        setProductsSold(currArr => [...currArr, productSold])
+
+
+        const saleAmount =  lineItem[3];
+        setDailySales(currArray => [...currArray, saleAmount]);
+      })
+      
+      
+    } catch (error) {
+      console.log('error reading this file');
+    }
+  }
+    
+  const getTotalAmount = () => {
+    const result = dailySales?.map(sale => parseFloat(sale)).filter(sale => !isNaN(sale))
+    if(result.length > 0){
+      const value = Math.max(...result)
+      setHighestSale(value)
+    }
+  }
+
+  const getSales = () => {
+    const values = productsSold?.flatMap(items => items?.split('|').map(item => {
+      const value = item.match(/\[(\d+):/);
+      return value ? parseInt(value[1], 10) : NaN;
+    }))
+    const test = values.filter(value => !isNaN(value))
+    const result = test.length > 0 ? Math.max(...test) : null;
+    setHighestQty(result)
+  }
+
+  console.log('highest sold is -> ', productsSold)
+  console.log('value for highest', highestQty)
 
   return (
     <>
+      <span> Accounting system</span>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {dailySales.length !== 0 
+          ? <div>
+              <span> gotten sales data </span>
+              {highestSale && (
+                <p> Highest Sale is: {highestSale} </p>
+              )}
+              <button onClick={getTotalAmount}> view highest sales volume </button>
+              {highestQty && (
+                <p> Highest Sale is: {highestQty} </p>
+              )}
+              <div>
+                <button onClick={getSales}> Check Highest Value </button>
+              </div>
+            </div>
+          : <span>sales amount should come here</span>
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
