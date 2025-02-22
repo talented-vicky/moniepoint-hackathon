8816a90ca5245e17343270644052ad7cd6dcbbfd
talented_vicky <victorotubure7@gmail.com>
2025-02-22 16:32:36 +0100

    installed tailwind and added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 
+interface dataPair {
+  id: number,
+  value: number
+}
 
 function App() {
   const [dailySales, setDailySales] = useState<string[]>([]);
   const [highestSale, setHighestSale] =  useState<number | null>(null);
   const [productsSold, setProductsSold] =  useState<string[]>([])
   const [highestQty, setHighestQty] = useState<number | null>(null);
+
+  const [highestQtyValue, setHighestQtyValue] = useState<number| null>(null);
+  const [highestQtyID, setHighestQtyID] = useState<number[] | null>(null);
 
   useEffect(() => {
     const dataCalls = [
@@ -22,7 +29,7 @@ function App() {
 
       const line = textFile.split('\n');
 
-      const total = line.map(items => {
+      line.map(items => {
         const lineItem = items.split(',')
         const salesStaffId =  lineItem[0];
         const transactionTime =  lineItem[1];
@@ -52,36 +59,94 @@ function App() {
 
   const getSales = () => {
     const values = productsSold?.flatMap(items => items?.split('|').map(item => {
-      const value = item.match(/\[(\d+):/);
-      return value ? parseInt(value[1], 10) : NaN;
+      const matchPattern = item.match(/\[(\d+):/);
+      return matchPattern ? parseInt(matchPattern[1], 10) : NaN;
     }))
     const test = values.filter(value => !isNaN(value))
     const result = test.length > 0 ? Math.max(...test) : null;
     setHighestQty(result)
   }
 
-  console.log('highest sold is -> ', productsSold)
-  console.log('value for highest', highestQty)
+  const getMax = () => {
+    // const filteredProductSold = productsSold?.filter(product => product !== null && product !== undefined)
+
+    const idValuePairs: dataPair[] = [];
+
+    productsSold?.forEach(items => {
+      if(typeof(items) === 'string'){
+        const item = items?.split('|');
+        item.forEach(item => {
+          const match = item.match(/\[(\d+):(\d+)\]/);
+          if(match){
+            const id = parseInt(match[1], 10);
+            const value = parseInt(match[2], 10);
+            idValuePairs.push({id, value})
+          }
+        })
+      }
+    })
+
+    if(idValuePairs.length > 0){
+      const maxValue = Math.max(...idValuePairs.map(item => item.value));
+      setHighestQtyValue(maxValue);
+
+      const IDs = idValuePairs.filter(item => item.value === maxValue).map(
+        item => item.id
+        )
+      setHighestQtyID(IDs);
+    }
+  }
+
+  // console.log('highest sold is -> ', productsSold)
+  // console.log('value for highest', highestQty)
 
   return (
     <>
-      <span> Accounting system</span>
+      <h1> Accounting system</h1>
       <div>
         {dailySales.length !== 0 
-          ? <div>
-              <span> gotten sales data </span>
-              {highestSale && (
-                <p> Highest Sale is: {highestSale} </p>
-              )}
-              <button onClick={getTotalAmount}> view highest sales volume </button>
-              {highestQty && (
-                <p> Highest Sale is: {highestQty} </p>
-              )}
-              <div>
-                <button onClick={getSales}> Check Highest Value </button>
+          ? <div style={{
+            display: 'flex',
+            height: '80vh',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'space-between'
+          }}>
+              <span style={{fontSize: '2rem'}}> -- Sales data Query -- </span>
+
+              <div style={{display: 'flex'}}>
+                <button onClick={getTotalAmount}> view highest sales volume </button>
+                {highestSale && ( <p> Highest Sale is: {highestSale} </p> )}
+              </div>
+              
+              <div style={{display: 'flex', alignItems: 'center'}}>
+                <div>
+                  <button onClick={getSales}> Check Highest Value </button>
+                </div>                
+                {highestQty && (
+                  <div >  Highest Sale is: {highestQty}  </div>
+                )}
+              </div>
+
+              <div style={{backgroundColor: 'teal', padding: '2rem', borderRadius: '30%'}}>
+                <div>
+                  <button onClick={getMax}> Check Max Value </button>
+                </div>
+                {highestQtyValue && (
+                  <div style={{
+                    display: 'flex',
+                    color: 'white',
+                    justifyContent: 'space-between'
+                  }}>
+                    <p> Highest Qty is: {highestQtyValue} </p>
+                    {highestQtyID?.map(item => (
+                      <p> Product ID : {item} </p>
+                    ))}
+                  </div>
+                )}
               </div>
             </div>
-          : <span>sales amount should come here</span>
+          : <span>No data provided yet</span>
         }
       </div>
     </>
